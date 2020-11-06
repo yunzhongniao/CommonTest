f21477a56367d4e35f7f857a28a94d18f2f63108
wanyunzhong@aliyun.com <wangyunzhong@aliyu.com>
2020-11-06 11:17:26 +0800

    添加详细参数的线程池构造接口。 添加销毁接口

--- src/main/java/org/yunzhong/CommonTest/util/multithread/ThreadPoolFactory.java
+++ src/main/java/org/yunzhong/CommonTest/util/multithread/ThreadPoolFactory.java
@@ -5,12 +5,18 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.util.StringUtils;
 import org.yunzhong.CommonTest.util.SpringUtil;
 import org.yunzhong.CommonTest.util.multithread.ThreadPoolsInfo.ThreadPoolInfo;
 
+/**
+ * @author yunzhong
+ *
+ */
 public class ThreadPoolFactory {
     public static final String DEFAULT_POOL = "DEFAULT_POOL";
 
@@ -47,12 +53,41 @@ public class ThreadPoolFactory {
     }
 
     /**
+     * @param poolName
+     * @param corePoolSize
+     * @param maximumPoolSize
+     * @param keepAliveTime
+     * @param queueSize
+     * @param handler
+     * @return
+     */
+    public static ThreadPoolExecutor getPool(String poolName, int corePoolSize, int maximumPoolSize, int keepAliveTime,
+            int queueSize, RejectedExecutionHandler handler) {
+        if (poolMap.containsKey(poolName)) {
+            return poolMap.get(poolName);
+        } else {
+            synchronized (poolMap) {
+                if (!poolMap.containsKey(poolName)) {
+                    if (threadPoolConfig == null) {
+                        threadPoolConfig = SpringUtil.getBean(ThreadPoolConfig.class);
+                    }
+                    poolMap.put(poolName,
+                            new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.MILLISECONDS,
+                                    new ArrayBlockingQueue<>(queueSize), new ThreadPoolThreadFactory(DEFAULT_POOL),
+                                    handler));
+                }
+            }
+            return poolMap.get(poolName);
+        }
+    }
+
+    /**
      * @return
      */
     public static ThreadPoolsInfo getInfo() {
         ThreadPoolsInfo info = new ThreadPoolsInfo();
         Iterator<Entry<String, ThreadPoolExecutor>> iterator = poolMap.entrySet().iterator();
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             Entry<String, ThreadPoolExecutor> next = iterator.next();
             String key = next.getKey();
             ThreadPoolExecutor value = next.getValue();
@@ -62,5 +97,32 @@ public class ThreadPoolFactory {
             info.appendWaitingSize(poolInfo.getWaitingSize());
         }
         return info;
+    }
+
+    public static void shutdown(String poolName) {
+        if (StringUtils.isEmpty(poolName)) {
+            return;
+        }
+        ThreadPoolExecutor threadPoolExecutor = poolMap.get(poolName);
+        if (threadPoolExecutor == null) {
+            return;
+        }
+        synchronized (poolMap) {
+            if (poolMap.containsKey(poolName)) {
+                poolMap.remove(poolName);
+                if (!threadPoolExecutor.isShutdown()) {
+                    threadPoolExecutor.shutdownNow();
+                }
+                boolean isShutdown = false;
+                try {
+                    isShutdown = threadPoolExecutor.awaitTermination(1000L, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (!isShutdown) {
+                    System.out.println("Invoke thread pool shutdown ,but raised exception .");
+                }
+            }
+        }
     }
 }
